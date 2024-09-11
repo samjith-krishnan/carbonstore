009243b2620c72f72dd8495ce84fff6ff756106f
samjith <samjithkrishna7510@gmail.com>
2024-09-11 21:06:51 +0530

    second commit

--- carbonstore/spiders/carbonspider.py
+++ carbonstore/spiders/carbonspider.py
@@ -6,24 +6,46 @@ class CarbonSpider(scrapy.Spider):
 
     def parse(self,response):
         for product in response.css('div.ProductItem'):
-            try:
-                yield{
-                    'name': product.css('h2.ProductItem__Title.Heading a::text').get(),
-                    'price': product.css('span.ProductItem__Price.Price::text').get().replace('Rs. ',''),
-                    'link': product.css('a.ProductItem__ImageWrapper.ProductItem__ImageWrapper--withAlternateImage').attrib['href'],
+            product_url= product.css('h2.ProductItem__Title.Heading a').attrib['href']
 
-                }
-            except:
-                yield{
-                    'name': product.css('h2.ProductItem__Title.Heading a::text').get(),
-                    'price': 'sold out',
-                    'link': product.css('a.ProductItem__ImageWrapper.ProductItem__ImageWrapper--withAlternateImage').attrib['href'],
+            if product_url:
+                yield response.follow(product_url, callback=self.parse_profile)
 
-                }
-
-        next_page= response.css('a.Pagination__NavItem.Link.Link--primary').attrib['href']
+        next_page= response.css('a[rel="next"]::attr(href)').get()
         if next_page is not None:
-            yield response.follow(next_page,callback=self.parse)
-                    
+          yield response.follow(next_page,callback=self.parse)
+
+
+
+
+    def parse_profile(self,response):
+                try:
+                    yield{
+                        'primary_image_url':response.css('div.AspectRatio.AspectRatio--withFallback img').attrib['src'],
+                        'brand': response.css('h2.ProductMeta__Vendor.Heading.u-h1 a::text').get(),
+                        'product_name': response.css('h1.ProductMeta__Title.Heading.u-h3::text').get(),
+                        'price': response.css('span.ProductMeta__Price.Price::text').get(),
+                        'reviews':response.css('div.yotpo-sr-bottom-line-text.yotpo-sr-bottom-line-text--right-panel::text').get(),
+                        'colour':response.css('span.ProductForm__SelectedValue::text').get(),
+                        'sizes':response.css('ul.SizeSwatchList.HorizontalList.HorizontalList--spacingTight li label::text').getall(),
+                        'description':response.css('div.Faq__Answer.Rte span::text').get(),
+                        'product_url': response.url,
+                        'image_urls':response.css('div.Product__SlideshowNavScroller a::attr(href)').getall(),
+
+
+                    }
+                except:
+                    yield{
+                        'primary_image_url':response.css('div.AspectRatio.AspectRatio--withFallback img').attrib['src'],
+                        'brand': response.css('h2.ProductMeta__Vendor.Heading.u-h1 a::text').get(),
+                        'product_name': response.css('h1.ProductMeta__Title.Heading.u-h3::text').get(),
+                        'price': 'out of stock',
+                        'reviews':'0 Reviews',
+                        'colour':'not available',
+                        'sizes':'out of stock',
+                        'description':response.css('div.Faq__Answer.Rte span::text').get(),
+                        'product_url': response.url,
+                        'image_urls':response.css('div.Product__SlideshowNavScroller a::attr(href)').getall(),
+                    }
 
         
